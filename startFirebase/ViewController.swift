596c5fbeab910742556a36bb6d7aa4d2985fd544
Mizuki Morohoshi <8080suisho@gmail.com>
2020-09-14 06:40:40 +0900

    Initial Commit

---
+++ startFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  startFirebase
+//
+//  Created by 諸星水晶 on 2020/09/14.
+//  Copyright © 2020 Mizuki Morohoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
